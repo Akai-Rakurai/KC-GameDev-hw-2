60fe3aebf0311bb4fae90dbff8c3097db6d3a9b9
Akai-Rakurai <Dryusufhacks@gmail.com>
2022-08-23 22:09:47 +0300

    Added Pong

---
+++ pong/Assets/Game_Start.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game_Start : MonoBehaviour
+{
+    public void gamestart()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
